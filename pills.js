9e9791ceae5027636c0f2444a22b5eefc5abf023
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-12-06 22:21:11 -0800

    update

--- pills.js
+++ pills.js
@@ -1,28 +1,28 @@
-function Pill(x, y){
+function Pill(x, y) {
   this.x = x;
   this.y = y;
   this.r = 6;
   this.toDelete = false;
-  
-  this.show = function (){
+
+  this.show = function() {
     noStroke();
     fill(245, 135, 190);
-    ellipse(this.x, this.y, this.r*2, this.r*2);
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
   }
-  
-  this.evaporate = function(){
+
+  this.evaporate = function() {
     this.toDelete = true;
   }
-  
-  this.hits = function(politician){
+
+  this.hits = function(politician) {
     var d = dist(this.x, this.y, politician.x, politician.y);
-    if (d < this.r + politician.r) { 
-    return true;
+    if (d < this.r + politician.r) {
+      return true;
     } else {
       return false;
     }
   }
-  
+
   this.move = function() {
     this.x = this.x + 3;
   }
